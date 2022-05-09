d0f2522a4059a0e05e0773028f215fcb7e367434
Choi YoungHun <young_h90@naver.com>
2022-05-09 23:43:36 +0900

    MUI Plugin Modules

---
+++ src/DM.ModuleOne/ModuleOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+
+namespace DM.ModuleOne
+{
+    public class ModuleOne : IModule
+    {
+        private readonly IUnityContainer _container;
+        
+        public ModuleOne(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException($"{nameof(container)}");
+            }
+
+            _container = container;
+        }
+
+        public void Initialize()
+        {
+            //_container.RegisterType<InterfaceName, ClassName>();
+        }
+    }
+}
---
+++ src/DM.ModuleTwo/ModuleTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+
+namespace DM.ModuleTwo
+{
+    public class ModuleTwo : IModule
+    {
+        private readonly IUnityContainer _container;
+
+        public ModuleTwo(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException($"{nameof(container)}");
+            }
+
+            _container = container;
+        }
+
+        public void Initialize()
+        {
+            //_container.RegisterType<InterfaceName, ClassName>();
+        }
+    }
+}
